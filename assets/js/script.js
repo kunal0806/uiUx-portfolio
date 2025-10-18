fff5c6d57f69346bb61a27047bfdbcbe7253028a
Kunal Gujjar <kunal00053@gmail.com>
2025-10-18 14:27:50 +0530

    Complete

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+console.log('here');
+const btn = document.getElementById('contact-btn');
+console.log(btn);
+
+if (btn) {
+  btn.addEventListener('click', () => {
+    btn.classList.add('scale-105');
+    setTimeout(() => {
+      btn.classList.remove('scale-105');
+      window.location.href = './contact.html';
+    }, 150);
+  });
+}
