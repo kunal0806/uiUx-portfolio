791445caa291368dcab985bfea660cc2b23ccbb6
Kunal Gujjar <kunal00053@gmail.com>
2025-10-19 17:09:35 +0530

    Correct Format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
-console.log('here');
-const btn = document.getElementById('contact-btn');
+console.log("here");
+const btn = document.getElementById("contact-btn");
 console.log(btn);
 
 if (btn) {
-  btn.addEventListener('click', () => {
-    btn.classList.add('scale-105');
+  btn.addEventListener("click", () => {
+    btn.classList.add("scale-105");
     setTimeout(() => {
-      btn.classList.remove('scale-105');
-      window.location.href = './contact.html';
+      btn.classList.remove("scale-105");
+      window.location.href = "./contact.html";
     }, 150);
   });
 }
 
-  AOS.init({
-    duration: 1000,
-    easing: 'ease-out-cubic',
-    once: true
-  });
+AOS.init({
+  duration: 1000,
+  easing: "ease-out-cubic",
+  once: true,
+});
